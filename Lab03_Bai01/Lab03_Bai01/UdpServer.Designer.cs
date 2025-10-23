8873b24eaf8dc75115662c0eb2c4392a0f6841b0
Quach Trong Hai Quan <haiquansu252005@gmail.com>
2025-10-23 15:58:46 +0700

    Bai 1

---
+++ Lab03_Bai01/Lab03_Bai01/UdpServer.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Lab03_Bai01
+{
+    partial class UdpServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbReceivedMessages = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(62, 20);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "8080";
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(301, 16);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(91, 31);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Received messages";
+            // 
+            // rtbReceivedMessages
+            // 
+            this.rtbReceivedMessages.Location = new System.Drawing.Point(25, 83);
+            this.rtbReceivedMessages.Name = "rtbReceivedMessages";
+            this.rtbReceivedMessages.ReadOnly = true;
+            this.rtbReceivedMessages.Size = new System.Drawing.Size(367, 218);
+            this.rtbReceivedMessages.TabIndex = 4;
+            this.rtbReceivedMessages.Text = "";
+            // 
+            // UdpServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 323);
+            this.Controls.Add(this.rtbReceivedMessages);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Name = "UdpServer";
+            this.Text = "UDP Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UdpServer_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbReceivedMessages;
+    }
+
+}
+
---
+++ Lab03_Bai01/Lab03_Bai01/UdpServer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03_Bai01
+{
+    public partial class UdpServer : Form
+    {
+        private System.Net.Sockets.UdpClient udpServer;
+        private Thread listenThread;
+
+        public UdpServer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int port = int.Parse(txtPort.Text);
+                udpServer = new System.Net.Sockets.UdpClient(port);
+                rtbReceivedMessages.AppendText($"Server started, listening on port {port}...\n");
+
+                // Vô hiệu hóa các control để không khởi động server nhiều lần
+                btnListen.Enabled = false;
+                txtPort.Enabled = false;
+
+                // Bắt đầu một luồng (thread) riêng để lắng nghe dữ liệu,
+                // tránh làm giao diện bị treo (blocking)
+                listenThread = new Thread(new ThreadStart(ListenForClients));
+                listenThread.IsBackground = true; // Luồng sẽ tự động kết thúc khi ứng dụng chính đóng
+                listenThread.Start();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid port number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ListenForClients()
+        {
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                while (true)
+                {
+                    // Chờ nhận dữ liệu từ client (đây là một lời gọi blocking)
+                    byte[] receivedBytes = udpServer.Receive(ref remoteIpEndPoint);
+                    string receivedData = Encoding.UTF8.GetString(receivedBytes); // Dùng UTF8 để hỗ trợ tiếng Việt
+                    string messageToShow = $"{remoteIpEndPoint}: {receivedData}\n";
+
+                    // Cập nhật giao diện từ một luồng khác một cách an toàn
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        rtbReceivedMessages.AppendText(messageToShow);
+                    });
+                }
+            }
+            catch (SocketException)
+            {
+                // Ngoại lệ này sẽ xảy ra khi UdpClient bị đóng bởi luồng chính,
+                // báo hiệu rằng luồng lắng nghe nên kết thúc.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Listening error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Đảm bảo đóng UdpClient và luồng khi form đóng lại
+        private void UdpServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (udpServer != null)
+            {
+                udpServer.Close();
+            }
+            // Không cần phải .Abort() luồng vì việc đóng UdpClient sẽ gây ra SocketException
+            // và làm cho vòng lặp trong luồng kết thúc một cách tự nhiên.
+        }
+    }
+}
