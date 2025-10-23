702cb44f54332c7a3973700b531bb26be8ea3a92
VDHieu0612 <voduyhieu0612@gmail.com>
2025-10-23 15:17:20 +0700

    Create Folder Lab03

---
+++ Lab03/Lab03_Bai03/TCPServer/ServerForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TCPServer
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.rtbListen = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(41, 28);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(177, 64);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(557, 28);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(177, 64);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // rtbListen
+            // 
+            this.rtbListen.Location = new System.Drawing.Point(41, 110);
+            this.rtbListen.Name = "rtbListen";
+            this.rtbListen.Size = new System.Drawing.Size(693, 440);
+            this.rtbListen.TabIndex = 2;
+            this.rtbListen.Text = "";
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 591);
+            this.Controls.Add(this.rtbListen);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnListen);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox rtbListen;
+    }
+}
+
